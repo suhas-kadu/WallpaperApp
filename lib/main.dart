1e7546302101f7be0a185186430b55f20262fba8
Suhas Kadu <kadusuhas72@gmail.com>
2020-09-19 18:08:22 +0530

    version 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:Wallpaper/views/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -8,6 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       title: "Wallpaper",
       home: HomePage(),
     );
